789633fac3b05a31aa28949419c57e569bcdc3df
ANASFARHANE <anasfarhane321@gmail.com>
2022-07-05 11:52:21 +0100

    first

--- src/main/java/com/example/hrproject/Entities/Diplome.java
+++ src/main/java/com/example/hrproject/Entities/Diplome.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class Diplome {
-
     private String diplome;
     private String typeDiplome;
     private String ecole;
---
+++ src/main/java/com/example/hrproject/modals/CollabDTO.java
@@ -0,0 +1,10 @@
+package com.example.hrproject.modals;
+
+import com.example.hrproject.Entities.Collaborateur;
+
+public class CollabDTO {
+    private Collaborateur collab;
+    private List<Competence> competences;
+    private List<Diplome> diplomes;
+
+}
